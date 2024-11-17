9a525216f654a7d4da40cb74721ca4555b1ea6aa
LuizProgHenri <luiz.henrique@arenafantasy.com>
2024-11-17 10:34:11 -0300

    Create Component Container

---
+++ src/components/layout/ContainerLayout.tsx
@@ -0,0 +1,5 @@
+const ContainerLayout = ({ children }: { children: React.ReactNode }) => {
+	return <div className="w-screen sm:w-[500px] sm:m-auto sm:relative sm:border-tertiary-300 border-2">{children}</div>;
+};
+
+export { ContainerLayout };
