d1798f8552bfd907312c6536aeb8d114db304caa
LuizProgHenri <luiz.henrique@arenafantasy.com>
2024-11-17 10:33:57 -0300

    Create Component HomeLayout

--- src/components/layout/HomeLayout.tsx
+++ src/components/layout/HomeLayout.tsx
@@ -5,6 +5,7 @@ import { NavigationTracker } from "../utils/NavigationTracker";
 const HomeLayout = () => {
 	return (
 		<section className="min-w-screen min-h-screen bg-tertiary-light">
+			<Header variant="transparent" />
 			<main>
 				<NavigationTracker />
 				<Outlet />
