4576db16f39fc9044c841bea95dae1ad5d037191
LuizProgHenri <luiz.henrique@arenafantasy.com>
2024-11-17 10:34:37 -0300

    Setup Icons in Index

---
+++ src/components/icons/index.tsx
@@ -0,0 +1,6 @@
+import { HomeIcon } from "./HomeIcon";
+import { MoonIcon } from "./MoonIcon";
+import { SettingsIcon } from "./SettingsIcon";
+import { SunIcon } from "./SunIcon";
+
+export { HomeIcon, MoonIcon, SettingsIcon, SunIcon };
